148c90b196a4b8798babfe30d858d4289936375c
kalashsingh18 <kalashchouhan939@gmail.com>
2024-04-22 01:58:05 +0530

    print the udates

--- router/auth.py
+++ router/auth.py
@@ -45,6 +45,7 @@ def create_access_token(data: dict):
 def verify_access_token(token: str, credentials_exception: HTTPException):
  
         print("Token:", token)
+        print(token)
         token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxMSwiZXhwIjoyMDczNzMwNjYzfQ.Ng9vi_fq7O2x6qLm8_-wludc3FOmXJEp46TkLRias-0"
         print(token)
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
